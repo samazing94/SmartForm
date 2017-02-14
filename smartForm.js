368fa43de841401dbe3bb86db18af76dbfc596b6
Samina <samina.jamil@outlook.com>
2017-02-14 21:39:48 +0000

    assignment 8 update

--- smartForm.js
+++ smartForm.js
@@ -10,13 +10,51 @@ var userData = {
 	email: '',
 	html: [],
 	css: [],
-	js: [],
+	js: [], 
 	htmlSkill: null,
 	cssSkill: null,
 	jsSkill: null,
 	position: "welcome"
 };
+//validations
+	$(document).ready(function() {
+		$('#qNext').click(function(e) {
+			var name = $('#name').val();
+			var email = $('#exampleInputEmail1');
+			
+			var nameRegex = /^[a-zA-Z]+$/;
+			var emailRegex =  /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+			if (name.length == 0) {
+				$('#head').text("* All fields are mandatory *");
+				$('#name').focus();
+				return false;
+			}
+			else if(!name.test(nameRegex) || name.length == 0) {
+				$('#head').text("* Please use alphabets only *");
+				$('#name').focus();
+				$("#qNext").prop('disabled');
+				return false;
+			}
+			else if(!email.test(nameRegex) || email.length == 0) {
+				$('#head').text("* Please enter a valid email address *");
+				$('#name').focus();
+				$("#qNext").prop('disabled');
+				return false;
+			}
+			else {
+	/*				$("#exampleInputEmail1").on("change keyup", function(){
+					console.log($(this).val());
+					$("#qNext").prop('disabled', $("#name").val() == '' || $("#exampleInputEmail1").val() == '');
+				});
 
+				$("#name").on("change", function(){
+					console.log($(this).val());
+					$("#qNext").prop('disabled', $("#name").val() == '' || $("#exampleInputEmail1").val() == '');
+				}); */
+				return true;
+			}
+		});
+	});
 var positionArray = [];
 //must use this kind of code to check if all buttons are enabled or not
 $("#qNext").prop('disabled', $("#name").val() == '' || $("#exampleInputEmail1").val() == '');
